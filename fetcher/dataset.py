2233823a4e7602b3224d2557ec718207dd50a165
vinzeebreak <vincenthoulbreque@gmail.com>
2018-10-22 20:24:52 +0200

    added gz handling + mnist

--- fetcher/dataset.py
+++ fetcher/dataset.py
@@ -1,5 +1,6 @@
 import os
 import io
+import gzip
 import zipfile
 
 import requests
@@ -61,9 +62,49 @@ class ZIPDataset:
 
         for i, url in enumerate(self.urls):
             print("{} / {} - {}".format(i+1, len(self.urls), url))
+
             r = requests.get(url, stream=True)
-            z = zipfile.ZipFile(io.BytesIO(r.content))
-            z.extractall(folder)
+            if r.status_code == 200:
+                z = zipfile.ZipFile(io.BytesIO(r.content))
+                z.extractall(folder)
+            else:
+                print("Failed downloading {}".format(url))
+
+    def __repr__(self):
+        return self.name
+
+
+class GZDataset:
+
+    def __init__(self, name, urls, save_path=None):
+        self.name = normalize_name(name)
+        self.urls = urls
+        self.save_path = save_path
+
+        if not os.path.exists(self.save_path):
+            os.makedirs(self.save_path)
+
+    def download(self):
+        print("Downloading {}...".format(self.name))
+
+        folder = os.path.join(self.save_path, self.name)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        for i, url in enumerate(self.urls):
+            print("{} / {} - {}".format(i+1, len(self.urls), url))
+            if len(self.urls) > 1:
+                f_name = "{}_{}.gz".format(self.name, i)
+            else:
+                f_name = "{}.gz".format(self.name)
+
+            r = requests.get(url, stream=True)
+            if r.status_code == 200:
+                with open(f_name, 'wb') as f:
+                    for chunk in r:
+                        f.write(chunk)
+            else:
+                print("Failed downloading {}".format(url))
 
     def __repr__(self):
         return self.name
