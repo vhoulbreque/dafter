2565e3051f3a7a69c95e022d1dc1d1a729ee1216
vinzeebreak <vincenthoulbreque@gmail.com>
2019-05-01 15:18:39 +0200

    Create config dirs when pip installing

--- setup.py
+++ setup.py
@@ -2,6 +2,33 @@
 # coding=utf-8
 
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+
+import os
+from os.path import expanduser
+
+
+def create_config_dirs():
+    HOME = expanduser("~")
+
+    DATABASE_DIR = os.path.join(HOME, ".dafter")
+    if not os.path.exists(DATABASE_DIR):
+        os.makedirs(DATABASE_DIR)
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        create_config_dirs()
+        develop.run(self)
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        create_config_dirs()
+        install.run(self)
+
 
 setup(
     name='dafter',
@@ -13,7 +40,7 @@ setup(
     'hours building models instead of looking for datasets and their urls.'),
     author='Vincent Houlbreque',
     url='https://github.com/vinzeebreak/dafter',
-    keywords=['dataset', 'datasets', 'cli'],
+    keywords=['dataset', 'datasets', 'cli', 'downloader'],
     entry_points={'console_scripts': ['dafter = dafter.cli:main']},
     install_requires=[
         'requests',
@@ -21,4 +48,8 @@ setup(
     ],
     packages=find_packages(),
     include_package_data=True,
-    license='Apache 2.0')
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
+    license='MIT')
